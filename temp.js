525a0cefde75796b77f34efd920297da70438b37
Satyam-03-Gupta <grinku126@gmail.com>
2024-11-04 22:46:18 +0530

    second-commit

--- temp.js
+++ temp.js
@@ -1,1 +1,7 @@
 console.log('hello, world!');
+
+
+// <div class="navicon" onclick="window.location.href='https://timetosweat.in'">
+//                 <img class="icon" src="photos/timetosweaticon.png" alt="reloading...">
+//                 <span class="sitename">imeto<span class="s">S</span>weat</span>
+//             </div>
